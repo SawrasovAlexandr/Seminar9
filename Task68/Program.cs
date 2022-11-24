a1c2a0c4a9779361fe7cd64a9119ebd436fb28a3
AlexandrSawrasov <caiiiuk@mail.ru>
2022-11-25 00:47:56 +0300

    add Task68 2

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -5,7 +5,7 @@
 int Ackerman(int m, int n)
 {
     if (m == 0) return n + 1;
-    if (m > 0 && n == 0) return Ackerman(m - 1, 1);
+    if (n == 0) return Ackerman(m - 1, 1);
     return Ackerman(m - 1, Ackerman(m, n - 1));
 }
 
